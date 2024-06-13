5c00e40220e35bb137d50ce9d95d0e75332ff023
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2024-06-12 19:46:47 -0600

    Create Transition

---
+++ Scenes/Transition/transition.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func transition_scene(target: String) -> void:
+	$AnimationPlayer.play("fade_animation")
+	await $AnimationPlayer.animation_finished
+	get_tree().change_scene_to_file(target)
+	$AnimationPlayer.play_backwards("fade_animation")
