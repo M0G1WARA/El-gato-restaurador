fa8d0db2b816b33fa61312a0631079e8171b6052
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2024-06-18 22:09:48 -0600

    Update map.gd

--- Scenes/Map/map.gd
+++ Scenes/Map/map.gd
@@ -1,5 +1,11 @@
 extends Control
 
+var scene = load("res://Scenes/RestorationGame/restoration_game.tscn")
+#var scene = preload("res://Scenes/RestorationGame/restoration_game.tscn")
+var instance = scene.instantiate()
+
+func _ready():
+	instance.connect("removeScene", _on_mi_signal)
 
 func _on_mountain_range_pressed():
 	Transition.show_transition()
@@ -15,4 +21,8 @@ func _on_store_pressed():
 	Transition.transition_scene("res://Scenes/Store/store.tscn")
 
 func _on_cave_painting_pressed():
-	Transition.transition_scene("res://Scenes/RestorationGame/restoration_game.tscn")
+	#Transition.transition_scene("res://Scenes/RestorationGame/restoration_game.tscn")
+	add_child(instance)
+	
+func _on_mi_signal():
+		remove_child(instance)
