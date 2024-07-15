845a7b0b337936fd3cb185072ea4589160a5261e
M0G1WARA <121844050+M0G1WARA@users.noreply.github.com>
2024-07-14 19:47:09 -0600

    Implement weight

--- Scenes/Item/item.gd
+++ Scenes/Item/item.gd
@@ -21,10 +21,12 @@ func _on_buy_button_pressed():
 			if Global.backpack.size() < 5 and Global.money>Global.products[productIndex]['price']:
 				Global.backpack.append_array([0])
 				$VBoxContainer/BuyButton.disabled = true
+				Global.weight += 1.0
 		1:
 			if Global.backpack.size() < 5 and Global.money>Global.products[productIndex]['price']:
 				Global.backpack.append_array([0,0,0])
 				$VBoxContainer/BuyButton.disabled = true
+				Global.weight += 3.0
 		2,3,4,5,6:
 			if 0 in Global.backpack and Global.money>Global.products[productIndex]['price']:
 				var index = Global.backpack.find(0)
